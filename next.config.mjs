26ce5d0a18f369d64393d9e4be282047df9a2bf7
Daniel González Briseño <qbixmex@gmail.com>
2024-07-13 13:16:18 -0700

    ShadCDN Installation and Configuration
    - Purple Theme
    - Dark Mode
    - Button Installed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      // Example
+      // {
+      //   protocol: 'https',
+      //   hostname: 's3.amazonaws.com',
+      //   port: '',
+      //   pathname: '/my-bucket/**',
+      // },
+    ],
+  },
+};
 
 export default nextConfig;
