026abc6f507ac5cc37c593ddaec3b515303c6965
Daniel González Briseño <qbixmex@gmail.com>
2024-07-18 18:44:19 -0700

    Article Improvement
    - [x] Tags added
    - [x] Published date added
    - [x] Category added
    - [x] Author added
    - [x] Share social media added
    - [x] Responsive design Fixed

---
+++ src/interfaces/article.ts
@@ -0,0 +1,13 @@
+export interface Article {
+  id: string;
+  title: string;
+  slug: string;
+  image: string;
+  description: string;
+  content: string;
+  category: string;
+  tags: string[];
+  publishedAt: string;
+  author: string;
+  robots: string;
+}
