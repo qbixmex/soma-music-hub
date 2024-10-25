28b277d7c5531833f83ca7324d03d4c134741516
Daniel González Briseño <qbixmex@gmail.com>
2024-10-24 22:14:05 -0700

    Fixed Event Status Optimic Update

--- src/app/admin/events/(components)/toggle-active.component.tsx
+++ src/app/admin/events/(components)/toggle-active.component.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { FC, useOptimistic } from 'react';
+import { FC, useOptimistic, startTransition } from 'react';
 import { Switch } from "@/components/ui/switch";
 import updateActiveEvent from "@/actions/events/toggle_status";
 import { toast } from "sonner";
@@ -14,33 +14,32 @@ const ToggleActive: FC<Props> = ({ id, currentStatus }) => {
 
   const [ optimisticStatus, addOptimisticStatus ] = useOptimistic(
     currentStatus,
-    (currentState, newStatus) => currentState !== newStatus,
+    (currentState) => !currentState
   );
 
   const onSwitchChange = async () => {
+    startTransition(async () => {
 
-    addOptimisticStatus(!currentStatus);
-
-    const response = await updateActiveEvent(id, !currentStatus);
-
-    if (!response.ok) {
-      // In case the request fails, we revert the optimistic status.
       addOptimisticStatus(currentStatus);
 
-      toast.error(response.message, {
-        duration: 3000,
-        position: "top-right",
-        className: "bg-red-500 text-white",
-      });
-    }
+      const response = await updateActiveEvent(id, !currentStatus);
 
-    if (response.ok) {
-      toast.success(response.message, {
-        duration: 3000,
-        position: "top-right",
-        className: "bg-green-500 text-white",
-      });
-    }
+      if (!response.ok) {
+        toast.error(response.message, {
+          duration: 3000,
+          position: "top-right",
+          className: "bg-red-500 text-white",
+        });
+      }
+  
+      if (response.ok) {
+        toast.success(response.message, {
+          duration: 3000,
+          position: "top-right",
+          className: "bg-green-500 text-white",
+        });
+      }
+    });
   };
 
   return (
